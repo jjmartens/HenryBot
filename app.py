222b8d2adaa49ce25ac35c68a02f04468ff21845
Jan <jan-martens@hotmail.com>
2015-06-25 16:20:55 +0200

    add commands

--- app.py
+++ app.py
@@ -1,15 +1,44 @@
 import requests
 import time
+import re
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
 
-API_KEY = "bot110574668:AAFWistE1uWqzrcqryg9U08FgYj7P94wBDo"
+engine = create_engine('mysql://root:@localhost/henry', pool_recycle=3600)
+Base = declarative_base()
 
+class Response(Base):
+     __tablename__ = 'users'
+
+     id = Column(Integer, primary_key=True)
+     trigger = Column(String)
+     response = Column(String)
+     def __init(self, trigger,response):
+         self.trigger = trigger
+         self.response = response
+
+     def __repr__(self):
+        return "<User(name='%s', fullname='%s', password='%s')>" % (
+                             self.name, self.fullname, self.password)
+
+API_KEY = "<GECENSUREERD>"
+
+dict = {}
 def parse_response(json):
     if 'text' in json['message']:
-        if "geld" in json['message']['text']:
-            requests.get("https://api.telegram.org/{}/sendMessage?chat_id={}&text=Geld?! je hebt nooit geen geld!".format(API_KEY, json['message']['chat']['id']))
-        if "willem" in json['message']['text']:
-            requests.get("https://api.telegram.org/{}/sendMessage?chat_id={}&text=Willem?! bedoel je Willy?".format(API_KEY, json['message']['chat']['id']))
+        match = re.search(r'(\w+):(\w+):(.+)', json['message']['text'])
+        if match is not None:
+            if(match.group(1) == "add"):
+                pattern = match.group(2)
+                answer = match.group(3)
+                if len(pattern)> 3:
+                    dict[pattern] = answer
 
+        for key in dict.keys():
+            if key in json['message']['text']:
+                requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(API_KEY, json['message']['chat']['id'], dict[key]))
 
 
 def loop(id = 0):
