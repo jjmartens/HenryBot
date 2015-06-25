503f630bc43c202600f3c987f1c0f8874424f476
Jan <jan-martens@hotmail.com>
2015-06-25 15:05:08 +0200

    init commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import requests
+import time
+
+API_KEY = "<CENSORED>"
+
+def parse_response(json):
+    print json
+    if 'text' in json['message']:
+        if "geld" in json['message']['text']:
+            requests.get("https://api.telegram.org/{}/sendMessage?chat_id={}&text=Geld?! je hebt nooit geen geld!".format(API_KEY, json['message']['chat']['id']))
+        if "willem" in json['message']['text']:
+            requests.get("https://api.telegram.org/{}/sendMessage?chat_id={}&text=Willem?! bedoel je Willy?".format(API_KEY, json['message']['chat']['id']))
+
+
+
+def loop(id = 0):
+    data = requests.get("https://api.telegram.org/{}/getUpdates?offset={}&timeout=30".format(API_KEY, id))
+    json = data.json()
+    result = json['result']
+    if len(result) > 0:
+        id = result[0]['update_id']
+        parse_response(result[0])
+    time.sleep(1)
+    loop(id + 1)
+
+if __name__ == '__main__':
+    loop()
+
