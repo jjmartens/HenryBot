ea9fd6411be29f1de2eda2682ca63ee6e538af4c
Jan <jan-martens@hotmail.com>
2015-06-25 16:44:16 +0200

    database persistance

--- app.py
+++ app.py
@@ -5,26 +5,27 @@ import sqlalchemy
 import os
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from sqlalchemy import Column, Integer, String
 
 engine = create_engine('mysql://root:@localhost/henry', pool_recycle=3600)
 Base = declarative_base()
-
+session = Session(engine)
 class Response(Base):
-     __tablename__ = 'users'
+     __tablename__ = 'Response'
 
      id = Column(Integer, primary_key=True)
      trigger = Column(String)
      response = Column(String)
-     def __init(self, trigger,response):
+     def __init__(self, trigger,response):
          self.trigger = trigger
          self.response = response
 
      def __repr__(self):
-        return "<User(name='%s', fullname='%s', password='%s')>" % (
-                             self.name, self.fullname, self.password)
+        return "<Response(trigger='%s', response='%s')>" % (
+                             self.trigger, self.response)
 
-API_KEY = os.environ('HENRY_API_KEY')
+API_KEY = os.environ['HENRY_API_KEY']
 
 dict = {}
 def parse_response(json):
@@ -34,9 +35,10 @@ def parse_response(json):
             if(match.group(1) == "add"):
                 pattern = match.group(2)
                 answer = match.group(3)
-                if len(pattern)> 3:
+                if len(pattern)> 2:
                     dict[pattern] = answer
-
+                    session.add(Response(pattern,answer))
+                    session.commit()
         for key in dict.keys():
             if key in json['message']['text']:
                 requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(API_KEY, json['message']['chat']['id'], dict[key]))
@@ -53,5 +55,8 @@ def loop(id = 0):
     loop(id + 1)
 
 if __name__ == '__main__':
+    q = session.query(Response).all()
+    for response in q:
+        dict[response.trigger] = response.response
     loop()
 
