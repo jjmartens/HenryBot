829824fda48961f24b7de9b107943f476083f86e
Nick Tönissen <n.tonissen@student.science.ru.nl>
2015-06-26 17:08:57 +0200

    init answer moved

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ def parse_response(json):
                 elif match.group(2) == "dump":
                     requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(API_KEY, json['message']['chat']['id'], ",".join(map(str,dict.keys())).encode('utf8')))
         else:
+            answer = ""
             for key in dict.keys():
-                answer = ""
                 if key.capitalize() in json['message']['text'].capitalize():
                     answer += dict[key].encode('utf-8')
                     answer += "\n"
