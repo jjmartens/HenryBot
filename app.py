f83f234458e08edb6cfa92714d4a69a7a409dccb
Nick Tönissen <n.tonissen@student.science.ru.nl>
2015-06-29 22:50:39 +0200

    Tab/ space fix

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ def parse_response(json):
         else:
             answer = ""
             for key in dict.keys():
-				rex = re.escape(key) + r'+(([?!.,:;]*$)|((\s.*)+))'
-				match = re.search(rex, json['message']['text'].upper(), re.IGNORECASE)
+                rex = re.escape(key) + r'+(([?!.,:;]*$)|((\s.*)+))'
+                match = re.search(rex, json['message']['text'].upper(), re.IGNORECASE)
                 if match
                     answer += dict[key]
                     answer += "\n"
